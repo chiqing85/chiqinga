49cadddb081fd6278a997da66dcfd6adc06f5f60
chiqing85 <33456991+chiqing85@users.noreply.github.com>
2018-01-30 01:11:07 +0800

    邮件配置修改

--- app/extra/mail.php
+++ app/extra/mail.php
@@ -3,7 +3,7 @@ return array (
   'SMTP' => 'smtp.163.com',
   'port' => '25',
   'email' => 'chiqing_85@163.com',
-  'pwd' => '622188niu',
+  'pwd' => 'xxx',
   'test_eamil' => 'qilingela@qq.com',
   'unlock' => '0',
 );
