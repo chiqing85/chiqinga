a4e83378740a29b590de4458bcd3a57d189e5e90
chiqing85 <33456991+chiqing85@users.noreply.github.com>
2018-04-15 23:54:43 +0800

    delete

--- runtime/cache/dd/302f94682dbd2a114d63b0433602e0.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000002592000
- exit();?>
-a:7:{i:0;a:2:{s:4:"name";s:12:"操作系统";s:5:"value";s:5:"WINNT";}i:1;a:2:{s:4:"name";s:9:"PHP版本";s:5:"value";s:6:"7.0.12";}i:2;a:2:{s:4:"name";s:11:"Mysql版本";s:5:"value";s:6:"5.5.53";}i:3;a:2:{s:4:"name";s:12:"运行环境";s:5:"value";s:12:"nginx/1.11.5";}i:4;a:2:{s:4:"name";s:12:"框架版本";s:5:"value";s:16:"ThinkPHP v5.0.11";}i:5;a:2:{s:4:"name";s:10:"gzip支持";s:5:"value";s:3:"YSE";}i:6;a:2:{s:4:"name";s:18:"最大上传限制";s:5:"value";s:2:"8M";}}
