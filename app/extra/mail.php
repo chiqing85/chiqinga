dc98e6a44f50c8a41053dc25632e403ae5e1c9c5
chiqing85 <33456991+chiqing85@users.noreply.github.com>
2018-04-17 19:05:59 +0800

    del

--- app/extra/mail.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array (
-    'SMTP' => 'smtp.163.com',
-    'port' => '25',
-    'email' => 'chiqing_85@163.com',
-    'pwd' => '622188niu',
-    'test_eamil' => 'qilingela@qq.com',
-    'unlock' => '0',
-);
